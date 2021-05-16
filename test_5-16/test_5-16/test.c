8ed89fb68ee9ac423f785b0157f2311479ba569c
gss-x <1039707439@qq.com>
2021-05-16 17:04:42 +0800

    提交代码

---
+++ test_5-16/test_5-16/test.c
@@ -0,0 +1,18 @@
+//包含一个叫stdio.h的文件
+//std-标准 standard input output
+#include<stdio.h>
+int main()
+//int是整型的意思 main前面的int表示main函数调用返回一个整型值
+//主函数-程序的入口-main函数有且仅有一个
+//库函数-C语言本身提供的函数，别人的东西-打招呼，
+//#include
+{
+	printf("hello world\n"	);
+	printf("hello world\n");
+	printf("hello world\n");
+	printf("hello 比特\n");
+	printf("hello world\n");
+	printf("hello world\n");
+	return 0;//返回整数，
+}
+//void main(){}  这种写法过时
